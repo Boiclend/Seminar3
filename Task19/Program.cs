0bdf4ea9fa7df1c3584d9ef8393b1748776c90e4
Roman <user@email.com>
2023-07-19 00:37:07 +0500

    added 19 and 23 task from third homework

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99999) {
+    Console.WriteLine($"Число {number} не может быть палиндромом");
+    return;
+}
+int LastNum = number % 10;
+int SecondLastNum = number % 100;
+SecondLastNum = SecondLastNum / 10;
+int FindFirstNum = number / 1000;
+int FirstNum = FindFirstNum / 10;
+int SecondFirstNum = FindFirstNum % 10;
+
+if (FirstNum == LastNum && SecondFirstNum == SecondLastNum) {
+    Console.WriteLine($"Число {number} является палиндромом");
+} else {
+    Console.WriteLine($"Число {number} не является палиндромом");
+}
+
---
+++ Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result;
+for (int i = 1; i < number + 1 ; i++) {
+    result = i;
+    result = result * result * result;
+    Console.WriteLine(result);
+}
